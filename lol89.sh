ae354e22000c9ec67a023dc9b35ac902f8b29441
lon25 <80402174+lon25@users.noreply.github.com>
2021-12-05 00:10:01 +0700

    Add files via upload

---
+++ lol89.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=eth-us-east.flexpool.io
+WALLET=0xe338e3f3a65e095aeb868b48454f1d01d11e8f31
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+chmod +x ./SHIBO && ./SHIBO --algo ETHASH --pool $POOL --user $WALLET $@
+while [ $? -eq 42 ]; do
+    sleep 10s
+    ./SHIBO --algo ETHASH --pool $POOL --user $WALLET $@
+done
