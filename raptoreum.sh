bf6230ac71317cfa49223df437a7f7dffff73f3b
lon25 <80402174+lon25@users.noreply.github.com>
2021-12-05 16:41:29 +0700

    Add files via upload

---
+++ raptoreum.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+./wildrig-multi --print-full --algo ghostrider --url stratum+tcps://eu.flockpool.com:5555 --user RXXg8vGFGrw9xz8YC8nYbvH5kCaFGtpzi8 --pass x
+sleep 5
+done
