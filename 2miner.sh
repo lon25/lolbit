a0ebbf79f8add0d5898703332fdb4be47e95ad5c
lon25 <80402174+lon25@users.noreply.github.com>
2022-03-14 21:37:09 +0700

    Create 2miner.sh

---
+++ 2miner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=eth.2miners.com:2020
+WALLET=0xe338e3f3a65e095aeb868b48454f1d01d11e8f31 
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+while [ $? -eq 42 ]; do
+    sleep 10s
+    ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+done
